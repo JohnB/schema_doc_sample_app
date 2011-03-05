a286a6de454187212c5705931e4f106463f644c2
John Baylor <john.baylor@gmail.com>
2011-03-05 15:46:07 -0800

    Initial commit for sample rails app for testing/demoing schema_doc plugin.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SchemaDocSampleApp::Application.config.secret_token = '160b6d39918e8d2672ffa6679f1d2df38bfba335d26e854e7b2fdda7f929e257303d065ba3b3b43a541b9b4b9150b18f0545b7cc1a26bd0f942f000bf976728a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SchemaDocSampleApp::Application.config.session_store :cookie_store, :key => '_schema_doc_sample_app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SchemaDocSampleApp::Application.config.session_store :active_record_store
