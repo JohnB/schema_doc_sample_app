56761120d610f2ad4c995357ad1d0cd5c0427f9d
John Baylor <john.baylor@gmail.com>
2011-03-06 13:07:48 -0800

    Added models and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 SchemaDocSampleApp::Application.routes.draw do
+  resources :loans
+
+  resources :toys
+
+  resources :children
+
+  resources :parents
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +56,7 @@ SchemaDocSampleApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "welcome#index"
 
   # See how all your routes lay out with "rake routes"
 
